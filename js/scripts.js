ad5cbdfd7c723b6ee1fe4ca8f32b950a5247c185
Yousun Kim <kimy595@newschool.edu>
2019-04-23 09:58:41 -0400

    change

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-unirest.get("https://musixmatchcom-musixmatch.p.rapidapi.com/wsr/1.1/track.lyrics.get?track_id=15449912")
-.header("X-RapidAPI-Host", "musixmatchcom-musixmatch.p.rapidapi.com")
-.header("X-RapidAPI-Key", "9fd3207ea8msh133b5d6e943f1e0p18afa5jsne2f7c62ca9f0")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
+
+(function (){
+  
+  const APIKEY='169f137f5fc6e376405ff0ec9b92f7ff',
+})
